e13560c1a77ec120119405729f7201656f71c795
AntonButov <58133570+AntonButov@users.noreply.github.com>
2025-04-02 21:20:19 +0300

    init commit

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.ktlint)
+}
+
+group = "gradle-tester"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.junit)
+    testImplementation(gradleTestKit())
+}
+tasks.named("build") {
+    dependsOn("ktlintFormat")
+}
+
+tasks.named("test") {
+    dependsOn("ktlintFormat")
+}
+
+ktlint {
+    debug.set(false)
+    android.set(false)
+    ignoreFailures.set(true)
+    reporters {
+        reporter(org.jlleitschuh.gradle.ktlint.reporter.ReporterType.PLAIN)
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
